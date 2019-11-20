16009166bca23e103d5f735598e1357aa70fd782
Gao JiaXiang <gaojxcs@gmail.com>
2019-11-20 04:50:18 -0800

    help shell

---
+++ curl.sh
@@ -0,0 +1,16 @@
+#curl 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1056&productId=1384071&score=0&sortType=5&page=29&pageSize=10&isShadowSku=0&fold=1' -H 'authority: sclub.jd.com' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36' -H 'accept: */*' -H 'sec-fetch-site: same-site' -H 'sec-fetch-mode: no-cors' -H 'referer: https://item.jd.com/1384071.html' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'cookie: shshshfpa=b9d489d2-82d7-6fe0-d66a-f7ee06d9c42c-1571790424; shshshfpb=plSFZFXZPndoBofuyNdfK8g%3D%3D; areaId=2; ipLoc-djd=2-2830-51810-0; 3AB9D23F7A4B3C9B=IX6LBDYQVYM36C3TXFX3M6E3DH5RRVLWWJ24LO4KCRUP6RTWWPJ6ZTZDEV2Y7OCFQ4LORTCFN4BHVKAIV6JP2DUOYI; __jdv=76161171|direct|-|none|-|1573551806061; PCSYCityID=CN_310000_310100_310115; __jdc=122270672; __jda=122270672.157179040436040842608.1571790404.1574210245.1574213572.12; shshshfp=0c6e36248008852a35296ebd8e2398e6; __jdu=157179040436040842608; JSESSIONID=AF9465CFCAA225A99F8E338EB78E31D8.s1; shshshsID=a37ab109c838a6ecfc26ce7def072149_7_1574218031940; __jdb=122270672.7.157179040436040842608|12.1574213572' --compressed
+
+#48&pageSize=10
+#curl 'https://sclub.jd.com/comment/productPageComments.action?productId=1384071&score=0&sortType=5&page=29&pageSize=10&isShadowSku=0&fold=1' \
+#	-H 'referer: https://item.jd.com/1384071.html' \
+#	--compressed
+
+
+
+curl 'https://sclub.jd.com/comment/productPageComments.action?productId=1384071&score=0&sortType=6&page=29&pageSize=100&isShadowSku=0&fold=0' \
+	-H 'referer: https://item.jd.com/1384071.html' \
+	--compressed
+
+#curl 'https://sclub.jd.com/comment/productPageComments.action?productId=1384071&score=0&sortType=6&page=29&pageSize=10&isShadowSku=0&rid=0&fold=1' \
+#	-H 'referer: https://item.jd.com/1384071.html' \
+#	--compressed
---
+++ es_connect.sh
@@ -0,0 +1,4 @@
+curl -u gaojx:anarkia7115 'https://portal4883-9.tangible-elastic-search-53.2839618036.composedb.com:34842/_cluster/health?pretty'
+#https://gaojx:anarkia7115@portal4883-9.tangible-elastic-search-53.2839618036.composedb.com:34842/
+
+
